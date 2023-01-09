bfc776fe903ed81952225b32c0f3d7e5106aca3f
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2023-01-09 13:01:53 +0700

    edit credit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer>Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
+    <footer>Inspirational quotes provided by <a href="https://api.quotable.io" target="_blank">Quotable API</a>
     </footer>
   )
 }
