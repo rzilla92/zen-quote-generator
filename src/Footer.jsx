408e764ef9fe9000f5141c5cdf20fc12744d0a7d
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2023-01-09 13:04:45 +0700

    edit credit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer>Inspirational quotes provided by <a href="https://api.quotable.io" target="_blank">Quotable API</a>
-    </footer>
+    <footer>Inspirational quotes provided by <a href="https://api.quotable.io" target="_blank">Quotable API</a></footer>
   )
 }
